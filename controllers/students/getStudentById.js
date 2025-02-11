42acc6b10a6f0e2348631681d395d171b97b6278
Elizabeth <lizagotovitryby@gmail.com>
2025-02-10 22:14:46 -0500

    updtaed students routes

--- controllers/students/getStudentById.js
+++ controllers/students/getStudentById.js
@@ -1,13 +1,16 @@
 import { Student } from "../../models/student.js";
+import { Observation } from "../../models/observation.js"; // убедитесь, что модель Observation импортируется
 import HttpError from "../../utils/HttpError.js";
 
 export const getStudentById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const student = await Student.findById(id);
+    let student = await Student.findById(id).lean();
     if (!student) {
-          throw HttpError(404, "Student not found");
+      throw HttpError(404, "Student not found");
     }
+
+   
     res.json(student);
   } catch (error) {
     next(error);
--- controllers/students/getStudentsByClass.js
+++ controllers/students/getStudentsByClass.js
@@ -6,19 +6,29 @@ export const getStudentsByClassId = async (req, res, next) => {
   try {
     const { classId } = req.params;
 
-    
+
     const students = await Student.find({
       "classes.classId": classId
 
     }).lean();
-      
-    const modifiedStudents = students.map(student => ({
-        ...student,
-        classes: student.classes.filter(cls => cls.classId.toString() === classId),
-        
-      }));
 
-    res.json(modifiedStudents);
+    const modifiedStudents = students.map(student => ({
+      ...student,
+      classes: student.classes.filter(cls => cls.classId.toString() === classId),
+
+    }));
+
+    const result = modifiedStudents.map(s => ({
+      _id: s._id,
+      firstName: s.firstName,
+      lastName: s.lastName,
+      gradeLevel: s.gradeLevel,
+      classGrade: s.classes[0].classGrade,
+      learningSkills: s.classes[0].observations,
+    }))
+
+ 
+    res.json(result);
   } catch (error) {
     next(error);
   }
